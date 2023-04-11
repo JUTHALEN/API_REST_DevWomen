44e5fa5e6bf79a8576aedf345b60320c8feb6587
llruiz <llanosrzm@gmail.com>
2023-04-11 17:36:09 +0200

    preparado para hacer testing controller

---
+++ src/test/java/com/proyecto/controllers/BootcampControllerTests.java
@@ -0,0 +1,67 @@
+package com.proyecto.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+
+import com.proyecto.entities.Bootcamp;
+import com.proyecto.services.BootcampService;
+import com.proyecto.utilities.FileDownloadUtil;
+import com.proyecto.utilities.FileUploadUtil;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import static org.mockito.ArgumentMatchers.any;
+// Para seguir el enfoque BDD con Mockito
+import static org.mockito.BDDMockito.given;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@SpringBootTest // levanta el contexto de Spring completamente para lo que necesitamos
+                // no como la anterior
+@AutoConfigureMockMvc
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+public class BootcampControllerTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private BootcampService bootcampService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private FileUploadUtil fileUploadUtil;
+
+    @MockBean
+    private FileDownloadUtil fileDownloadUtil;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @BeforeEach
+    public void setUp() {
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+    }
+
+}
